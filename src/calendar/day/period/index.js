6be9e75045464e78afc83733a98879e95eba390e
Stevanus_yohanes <stevanus.yohanesvcn@gmail.com>
2018-01-25 14:26:19 +0700

    st

--- src/calendar/day/period/index.js
+++ src/calendar/day/period/index.js
@@ -1,11 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 //import _ from 'lodash';
-import {
-  TouchableWithoutFeedback,
-  Text,
-  View
-} from 'react-native';
+import { TouchableWithoutFeedback, Text, View } from 'react-native';
 
 import * as defaultStyle from '../../../style';
 import styleConstructor from './style';
@@ -22,12 +18,12 @@ class Day extends Component {
     onPress: PropTypes.func,
     date: PropTypes.object,
 
-    markingExists: PropTypes.bool,
+    markingExists: PropTypes.bool
   };
 
   constructor(props) {
     super(props);
-    this.theme = {...defaultStyle, ...(props.theme || {})};
+    this.theme = { ...defaultStyle, ...(props.theme || {}) };
     this.style = styleConstructor(props.theme);
     this.markingStyle = this.getDrawingStyle(props.marking || []);
     this.onDayPress = this.onDayPress.bind(this);
@@ -54,7 +50,7 @@ class Day extends Component {
   }
 
   getDrawingStyle(marking) {
-    const defaultStyle = {textStyle: {}};
+    const defaultStyle = { textStyle: {} };
     if (!marking) {
       return defaultStyle;
     }
@@ -63,7 +59,7 @@ class Day extends Component {
     } else if (this.props.marking.selected) {
       defaultStyle.textStyle.color = this.theme.selectedDayTextColor;
     }
-    const resultStyle = ([marking]).reduce((prev, next) => {
+    const resultStyle = [marking].reduce((prev, next) => {
       if (next.quickAction) {
         if (next.first || next.last) {
           prev.containerStyle = this.style.firstQuickAction;
@@ -164,10 +160,10 @@ class Day extends Component {
           backgroundColor: flags.endingDay.color
         });
       } else if (flags.day) {
-        leftFillerStyle = {backgroundColor: flags.day.color};
-        rightFillerStyle = {backgroundColor: flags.day.color};
+        leftFillerStyle = { backgroundColor: flags.day.color };
+        rightFillerStyle = { backgroundColor: flags.day.color };
         // #177 bug
-        fillerStyle = {backgroundColor: flags.day.color};
+        fillerStyle = { backgroundColor: flags.day.color };
       } else if (flags.endingDay && flags.startingDay) {
         rightFillerStyle = {
           backgroundColor: this.theme.calendarBackground
@@ -182,8 +178,8 @@ class Day extends Component {
 
       fillers = (
         <View style={[this.style.fillers, fillerStyle]}>
-          <View style={[this.style.leftFiller, leftFillerStyle]}/>
-          <View style={[this.style.rightFiller, rightFillerStyle]}/>
+          <View style={[this.style.leftFiller, leftFillerStyle]} />
+          <View style={[this.style.rightFiller, rightFillerStyle]} />
         </View>
       );
     }
@@ -194,6 +190,11 @@ class Day extends Component {
           {fillers}
           <View style={containerStyle}>
             <Text style={textStyle}>{String(this.props.children)}</Text>
+          </View>
+          <View>
+            <View>
+              <Text style={this.props.textPriceStyle == null ? {} : this.props.textPriceStyle}>{this.props.price.toString().slice(0, -3)}</Text>
+            </View>
           </View>
         </View>
       </TouchableWithoutFeedback>
